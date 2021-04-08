45dcf0f69b1164f361e9fa5eb1cb8cf0b6c592ea
Lorenzo Ilardi <Lorenzo.Ilardi@fornitori.esselunga.it>
2021-04-08 14:41:28 +0200

    Aggiunto xib e .swift relativo

---
+++ RadioButton/Views/RadioButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  RadioButtonView.swift
+//  RadioButton
+//
+//  Created by Lorenzo Ilardi on 08/04/21.
+//
+
+import UIKit
+
+class RadioButtonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
